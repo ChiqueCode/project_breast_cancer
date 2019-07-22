56ba75d417c496d670bb9d01acceae783f450ba0
Arjun Subramaniam <arjunmbp@Arjuns-MBP.fios-router.home>
2019-07-22 14:29:43 -0400

    Updates to demo page

--- static/js/app.js
+++ static/js/app.js
@@ -1,71 +1,78 @@
 function createDropdown() {
-
-    /**
+  /**
     /* Creates dropdown menu of patient ID's
     */
 
-    // Create list of patient ID's
-    var patientList = []
-    for (var i = 19000; i <= 19010; i++) {
-        patientList.push(i);
-    }
+  // Create list of patient ID's
+  var patientList = [];
+  for (var i = 19000; i <= 19010; i++) {
+    patientList.push(i);
+  }
 
-    // Add first dropdown item
+  // Add first dropdown item
+  d3.select("#selPatient")
+    .append("option")
+    .text("");
+
+  // Create dropdown menu of patientID's to populate the select options
+  patientList.forEach(patientID => {
     d3.select("#selPatient")
-        .append("option")
-        .text("");
-
-    // Create dropdown menu of patientID's to populate the select options
-    patientList.forEach((patientID) => {
-        d3.select("#selPatient")
-            .append("option")
-            .text(patientID)
-            .property("value", patientID);
-    });
+      .append("option")
+      .text(patientID)
+      .property("value", patientID);
+  });
 }
 
+//Connects analyze function to button and passes in patientID
+d3.select("#analyze")
+    .on("click", function(d){
+        d3.event.preventDefault();
+        console.log(this.value);
+        selectPatient(this.value);
+        analyze(this.value);
+    });
+// document.getElementById("analyze").addEventListener("click", function(event) {
+//   event.preventDefault();
+//   console.log("working");
+// });
+function submitdropdownvalue(newvalue){
+    d3.select("#analyze").property("value", newvalue);
+} 
 function selectPatient(patientID) {
-
-    /**
+  /**
     /* Populates form with features from selected patient
     /* @param {string}    patientID    ID of selected patient 
     /* patient in feature array
     */
 
-    // Connects analyze function to button and passes in patientID
-    d3.select("#analyze")
-        .on("click", analyze(patientID));
+  // Clear values for existing feature table and diagnosis
+  d3.select("tbody").html("");
+  d3.select("#diagnosis").html("");
 
-    // Clear values for existing feature table and diagnosis
-    d3.select("tbody").html("");
-    d3.select("#diagnosis").html("");
+  var url = `/features/${patientID}`;
 
-    var url = `/features/${patientID}`;
-
-    // Fetch dictionary of the name of the features and corresponding values
-    d3.json(url).then(function (patientFeatures) {
-
-        // For each feature, enter the feature name and the feature value into a row
-        Object.entries(patientFeatures).forEach(([key, value]) => {
-            var tableRow = d3.select("tbody").append("tr");
-            tableRow.append("td").text(key);
-            tableRow.append("td").text(value);
-        });
+  // Fetch dictionary of the name of the features and corresponding values
+  d3.json(url).then(function(patientFeatures) {
+    // For each feature, enter the feature name and the feature value into a row
+    Object.entries(patientFeatures).forEach(([key, value]) => {
+      var tableRow = d3.select("tbody").append("tr");
+      tableRow.append("td").text(key);
+      tableRow.append("td").text(value);
     });
+  });
 }
 
 function analyze(patientID) {
-
-    /**
+  /**
     /* Calculates diagnosis of given patient and populates corresponding field
     /* @param {string}    patientID    ID of selected patient 
     */
 
-    var url = `/analyze/${patientID}`;
+  var url = `/analyze/${patientID}`;
 
-    d3.json(url).then(function (diagnosis) {
-        d3.select("#diagnosis").html(`Diagnosis: ${diagnosis}`)
-    });
+  d3.json(url).then(function(diagnosis) {
+    d3.select("#diagnosis").html(`Diagnosis: ${diagnosis}`);
+  });
 }
 
 createDropdown();
@@ -80,7 +87,6 @@ createDropdown();
 
 //     // Clear all input and output data
 
-
 //     // Fetch list of random data and populate associated fields
 //     d3.json("/random").then(function (sampleData) {
 //         for (var i = 0; i < sampleData.length; i++) {
@@ -88,8 +94,6 @@ createDropdown();
 //         };
 //     });
 // }
-
-
 
 // function reset() {
 
@@ -114,4 +118,3 @@ createDropdown();
 //             .html("");
 //     });
 // }
-
