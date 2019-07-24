0f334b85c6c67727e3a59539265338f812e8d412
Gretel <gretelup@gmail.com>
2019-07-24 13:51:06 -0400

    Cleanup

--- app.py
+++ app.py
@@ -1,64 +1,72 @@
 # Import dependencies
-import csv
-import os
-import pandas as pd
-import random
 import numpy as np
 from flask import (
     Flask,
     render_template,
-    request,
     jsonify)
 from joblib import load
 from sklearn.datasets import load_breast_cancer
 
-#Set up Flask
+# Set up Flask
 app = Flask(__name__)
 
 
 # home route
 @app.route("/")
 def index():
+    """Renders homepage"""
+
     return render_template("index.html")
 
 
 # vizualisations/tableau route
 @app.route("/story")
 def story():
+    """Renders visualizations page"""
+
     return render_template("story.html")
 
 
-# case studies route
 @app.route("/cases")
 def cases():
+    """Renders case studies page"""
+
     return render_template("cases.html")
 
 
-# demo/calculator route
 @app.route("/calculator")
 def calc():
+    """Renders demo page"""
+
     return render_template("calculator.html")
 
 
-# call to action/request assesment route 
 @app.route("/cta")
 def cta():
+    """Renders call to action page"""
+
     return render_template("cta.html")
-   
+
 
 @app.route("/features/<patientID>")
 def features(patientID):
     """Returns list of features for given patient ID"""
 
-
     # Create list of feature names
-    feature_names = ["Radius (worst)", "Texture (worst)", "Perimeter (worst)",\
-        "Area (worst)", "Smoothness (worst)", "Compactness (worst)", \
-        "Concavity (worst)", "Concave points (worst)", "Symmetry (worst)", \
+    feature_names = [
+        "Radius (worst)",
+        "Texture (worst)",
+        "Perimeter (worst)",
+        "Area (worst)",
+        "Smoothness (worst)",
+        "Compactness (worst)",
+        "Concavity (worst)",
+        "Concave points (worst)",
+        "Symmetry (worst)",
         "Fractal dimension (worst)"]
-    
+
     row = int(patientID) - 19000
-   
+
     # Load dataset from sklearn and set X to feature array
     X = load_breast_cancer().data
     feature_values = X[row]
@@ -74,13 +82,13 @@ def features(patientID):
 
 @app.route("/analyze/<patientID>")
 def analyze(patientID):
-    """Submit data to calculator"""
-
+    """Analyzes data for given patient using machine learning model
+    and returns diagnosis"""
 
     # Translate patient ID to row
     row = (int(patientID) - 19000)
 
-    # Load features, model, and scaler 
+    # Load features, model, and scaler
     X = load_breast_cancer().data
     model = load("cancer_model.joblib")
     scaler = load("scaler.out")
@@ -99,6 +107,6 @@ def analyze(patientID):
 
     return jsonify(diagnosis)
 
+
 if __name__ == "__main__":
-    # TODO: Remeber to turn debugging off when going live! 
-    app.run(debug=True, port=8000, host="localhost", threaded=True)
+    app.run(debug=False, port=8000, host="localhost", threaded=True)
