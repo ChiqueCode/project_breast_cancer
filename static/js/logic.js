e975292ffd5f26de91b6a744ffbf405858934b4c
Kseniya Bogoslovskaya <fromkseniya@gmail.com>
2019-07-24 17:33:43 -0400

    📚 for map: html, logic.js, config.js, heatmap plugin

---
+++ static/js/logic.js
@@ -0,0 +1,15 @@
+// Create a map object
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5
+  });
+  
+  // Add a tile layer
+  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
+  
